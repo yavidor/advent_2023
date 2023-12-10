331ad56cdcec72cd9c459339c33eeee8247468cb
yavidor <yonatan@avidor.org>
2023-12-10 22:35:30 +0200

    Finished part two

--- day2/main.go
+++ day2/main.go
@@ -25,14 +25,32 @@ func isOverMax(gameBalls map[string][]int) bool {
 	return true
 }
 
+func getMaximum(nums []int) int {
+	max := nums[0] - 1
+	for _, num := range nums {
+		if num > max {
+			max = num
+		}
+	}
+	return max
+}
+
+func getPower(gameBalls map[string][]int) int {
+	product := 1
+	for _, balls := range gameBalls {
+		product *= getMaximum(balls)
+	}
+	return product
+}
+
 func main() {
 	sum := 0
-	var reBalls = regexp.MustCompile(`(?m)(\d{1,2} \w{3,5})`)
-	var reNums = regexp.MustCompile(`(?m)\d+`)
-	var reChars = regexp.MustCompile(`(?m)[a-zA-Z]+`)
+	powerSum := 0
+	reBalls := regexp.MustCompile(`(?m)(\d{1,2} \w{3,5})`)
+	reNums := regexp.MustCompile(`(?m)\d+`)
+	reChars := regexp.MustCompile(`(?m)[a-zA-Z]+`)
 	file_data, err := fs.ReadFile(os.DirFS("."), "input.txt")
 	if err != nil {
-		fmt.Println(err)
 		panic(err)
 	}
 	var gamesArr []string = strings.Split(string(file_data), "\n")
@@ -53,7 +71,9 @@ func main() {
 		if isValid {
 			sum += gameIndex + 1
 		}
+		powerSum += getPower(gameBalls)
 
 	}
 	fmt.Println(sum)
+	fmt.Println(powerSum)
 }
