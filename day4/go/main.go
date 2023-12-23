77b02ef7b7943c8bd4b6f3bfe988954e1088bb60
yavidor <yonatan@avidor.org>
2023-12-23 12:49:23 +0200

    Part 1

--- day4/go/main.go
+++ day4/go/main.go
@@ -4,9 +4,18 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
+	"reflect"
 	"strings"
 )
 
+func contains[T any](s []T, v T) bool {
+	for _, vS := range s {
+		if reflect.DeepEqual(vS, v) {
+			return true
+		}
+	}
+	return false
+}
 func makeLines(data []byte) [][][]string {
 	inputLines := strings.Split(string(data), "\n")
 	outputLines := make([][][]string, 0, len(inputLines))
@@ -22,11 +31,27 @@ func makeLines(data []byte) [][][]string {
 	}
 	return outputLines
 }
+func calcCardValue(card [][]string) int {
+	ret := 1
+	for _, v := range card[1] {
+		if contains(card[0], v) {
+			ret *= 2
+		}
+	}
+	return ret / 2
+}
+func partOne(lines [][][]string) int {
+	sum := 0
+	for _, v := range lines {
+		sum += calcCardValue(v)
+	}
+	return sum
+}
 func main() {
-	fileData, err := fs.ReadFile(os.DirFS(".."), "example.txt")
+	fileData, err := fs.ReadFile(os.DirFS(".."), "input.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 	var lines [][][]string = makeLines(fileData)
-	fmt.Println(lines)
+	fmt.Println(partOne(lines))
 }
