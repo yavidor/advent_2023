1c8b29762d39d9af2eef642d73a644e5d63d914c
yavidor <yonatan@avidor.org>
2023-12-23 18:09:21 +0200

    Solved part 2

--- day4/go/main.go
+++ day4/go/main.go
@@ -17,6 +17,17 @@ func contains[T any](s []T, v T) bool {
 	}
 	return false
 }
+func removeIndex[T any](s []T, index int) []T {
+	return append(s[:index], s[index+1:]...)
+}
+func indexOf[T any](s []T, v T) int {
+	for i, vS := range s {
+		if reflect.DeepEqual(vS, v) {
+			return i
+		}
+	}
+	return -1
+}
 
 type Card struct {
 	winningNumbers []string
@@ -64,6 +75,23 @@ func partOne(cards []Card) int {
 	return sum
 }
 
+func partTwo(cards []Card) int {
+	var processedCards []int = make([]int, len(cards))
+	for i := range processedCards {
+		processedCards[i] = 1
+	}
+	for i, v := range cards {
+		for j := 0; j < v.value; j++ {
+			processedCards[i+j+1] += processedCards[i]
+		}
+	}
+	sum := 0
+	for _, v := range processedCards {
+		sum += v
+	}
+	return sum
+}
+
 func main() {
 	fileData, err := fs.ReadFile(os.DirFS(".."), "input.txt")
 	if err != nil {
@@ -72,4 +100,5 @@ func main() {
 	var lines [][][]string = makeLines(fileData)
 	var cards []Card = makeCards(lines)
 	fmt.Println(partOne(cards))
+	fmt.Println(partTwo(cards))
 }
